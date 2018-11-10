0c264c0d0db95926d9157047d74da12c1ab46975
Calin Marian Gacea <m.c.gacea@gmail.com>
2018-11-10 16:18:20 +0100

    aggiunta possibilità di eseguire script su più sottoreti

--- ping.sh
+++ ping.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-subnet=$1
-if [ -z "$subnet"]; then
-    echo "Inserisci subnet "
-    read subnet
-fi
-for i in $(seq 1 254)
+for j in $*
 do
-    ping -c1 -t 1 $subnet.$i
+subnet=$j
+    for i in $(seq 1 254)
+    do
+        ping -c 1 -t 1 $subnet.$i
+    done
 done
